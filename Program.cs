c0c2d7420310089e75256aa9ba49fc582e462398
silva guimaraes <silva__guimaraes@hotmail.com>
2024-04-03 20:47:40 -0300

    conex̧ão com postgres utilizando entity framework

--- Program.cs
+++ Program.cs
@@ -1,5 +1,36 @@
 
 
+// using System;
+// using System.Linq;
+
+// using var db = new BloggingContext();
+//
+// // Note: This sample requires the database to be created before running.
+// // Console.WriteLine($"Database path: {db.DbPath}.");
+//
+// // Create
+// Console.WriteLine("Inserting a new blog");
+// db.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+//
+// // Read
+// Console.WriteLine("Querying for a blog");
+// var blog = db.Blogs
+//     .OrderBy(b => b.BlogId)
+//     .First();
+//
+// // Update
+// Console.WriteLine("Updating the blog and adding a post");
+// blog.Url = "https://devblogs.microsoft.com/dotnet";
+// blog.Posts.Add(
+//     new Post { Title = "Hello World", Content = "I wrote an app using EF Core!" });
+//
+// // Delete
+// Console.WriteLine("Delete the blog");
+//
+// db.SaveChanges();
+
+
+
 
 var builder = WebApplication.CreateBuilder(args);
 
