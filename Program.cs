4e08cb9c26855ebe44474e561ea5c5290c383beb
silva guimaraes <silva__guimaraes@hotmail.com>
2024-05-05 15:17:11 -0300

    desfazer alterações do JWT

--- Program.cs
+++ Program.cs
@@ -1,45 +1,14 @@
 
 
 using Microsoft.EntityFrameworkCore;
-
-using System.Text;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-// using Microsoft.AspNetCore.Builder;
-// using Microsoft.AspNetCore.Hosting;
-// using Microsoft.Extensions.Configuration;
-// using Microsoft.Extensions.DependencyInjection;
-// using Microsoft.Extensions.Hosting;
-using Microsoft.IdentityModel.Tokens;
-
+using aspnet2.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddCors();
-// builder.Services.AddControllers();
 
-var key = Encoding.ASCII.GetBytes("foobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobarfoobar");
-builder.Services.AddAuthentication(x =>
-        {
-            x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-            x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-        })
-    .AddJwtBearer(x =>
-        {
-            x.RequireHttpsMetadata = false;
-            x.SaveToken = true;
-            x.TokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(key),
-                ValidateIssuer = false,
-                ValidateAudience = false
-            };
-        });
-
-
-// Add services to the container.
 builder.Services.AddControllersWithViews();
-builder.Services.AddDbContext<aspnet2.Models.MyDbContext>(options => 
+builder.Services.AddDbContext<MyDbContext>(options => 
         options.UseNpgsql(builder.Configuration.GetConnectionString("TestDatabase")));
 builder.Services.AddMvc();
 builder.Services.AddSwaggerGen();
@@ -55,19 +24,7 @@ if (!app.Environment.IsDevelopment())
 
 }
 
-// app.UseHttpsRedirection();
-// app.UseRouting();
-
-app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-
-// app.UseEndpoints(endpoints =>
-//         {
-//         endpoints.MapControllers();
-//         });
+// app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
 app.UseSwagger();
 app.UseSwaggerUI();
